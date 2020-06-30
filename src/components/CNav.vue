0908771c0f9a70b3613299a9db62abea558a309a
Daniel <dfsantosbu@unal.edu.co>
2020-06-30 10:49:13 +0200

    chore: crop to content image.

--- src/components/CNav.vue
+++ src/components/CNav.vue
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto px-2 sm:px-6">
       <div class="relative flex items-center justify-between h-16 fixed">
         <div class="flex-1 flex items-baseline justify-center sm:items-stretch sm:justify-start">
-          <img src="@/assets/img/logo_white.png" class="mr-2" width="40rem" />
+          <img src="@/assets/img/logo_white.png" class="mr-2" width="55rem" />
           <div class="flex-shrink-0 py-1 mr-2 text-lg text-white font-bold">
             SuperBowl
           </div>
