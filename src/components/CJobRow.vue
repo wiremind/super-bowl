68eebdbd9f1fb6caca025901cee875858d251215
Jonathan Griffe <jonathan.griffe@gmail.com>
2021-09-27 12:43:19 +0200

    feat: add inputs for args, kwargs and tz

--- src/components/CJobRow.vue
+++ src/components/CJobRow.vue
@@ -30,16 +30,16 @@
       </select>
     </td>
     <td class="border px-4 py-2">
-      <pre>{{ args | json }}</pre>
+      <input v-model="editableArgs" :class="{ 'invalid-input': !isArgsValid }" />
     </td>
     <td class="border px-4 py-2">
-      <pre>{{ kwargs | json }}</pre>
+      <input v-model="editableKwargs" :class="{ 'invalid-input': !isKwargsValid }" />
     </td>
     <td class="border px-4 py-2">
       {{ lastQueued | formatDistance }}
     </td>
     <td class="border px-4 py-2">
-      {{ tz }}
+      <input v-model="editableTz" />
     </td>
     <td class="border px-4 w-64">
       <div class="space-x-2 flex" v-if="isEdited && !errorMessage">
@@ -70,7 +70,13 @@ export default {
     isEdited: Boolean
   },
   data() {
-    return { errorMessage: null };
+    return {
+      errorMessage: null,
+      isArgsValid: true,
+      isKwargsValid: true,
+      editableArgs: null,
+      editableKwargs: null
+    };
   },
   filters: {
     formatDistance(time) {
@@ -131,6 +137,14 @@ export default {
       set(val) {
         this.$emit('update:isoWeekday', val);
       }
+    },
+    editableTz: {
+      get() {
+        return this.tz;
+      },
+      set(val) {
+        this.$emit('update:tz', val);
+      }
     }
   },
   methods: {
@@ -140,8 +154,52 @@ export default {
         setTimeout(() => (this.errorMessage = null), 3000);
         return;
       }
+      if (!this.isArgsValid) {
+        this.errorMessage = 'Invalid Args';
+        setTimeout(() => (this.errorMessage = null), 3000);
+        return;
+      }
+      if (!this.isKwargsValid) {
+        this.errorMessage = 'Invalid Kwargs';
+        setTimeout(() => (this.errorMessage = null), 3000);
+        return;
+      }
       this.$emit('save');
     }
+  },
+  watch: {
+    args(newValue) {
+      if (
+        utils.isJson(this.editableArgs) &&
+        JSON.stringify(JSON.parse(this.editableArgs)) !== JSON.stringify(newValue)
+      ) {
+        this.editableArgs = JSON.stringify(newValue);
+      }
+    },
+    editableArgs(val) {
+      this.isArgsValid = utils.isJson(val) && Array.isArray(JSON.parse(val));
+      if (this.isArgsValid) {
+        this.$emit('update:args', JSON.parse(val));
+      }
+    },
+    kwargs(newValue) {
+      if (
+        utils.isJson(this.editableKwargs) &&
+        JSON.stringify(JSON.parse(this.editableKwargs)) !== JSON.stringify(newValue)
+      ) {
+        this.editableKwargs = JSON.stringify(newValue);
+      }
+    },
+    editableKwargs(val) {
+      this.isKwargsValid = utils.isJson(val) && !Array.isArray(JSON.parse(val));
+      if (this.isKwargsValid) {
+        this.$emit('update:kwargs', JSON.parse(val));
+      }
+    }
+  },
+  created() {
+    this.editableArgs = JSON.stringify(this.args);
+    this.editableKwargs = JSON.stringify(this.kwargs);
   }
 };
 </script>
